5f8fee701879255f53e96c68ad4bad04d6dc74ec
InnaTymoshenko <innatymosh@gmail.com>
2024-07-05 14:41:31 +0300

    minor fixed

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,51 +1,39 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Button from '../Button/Button'
-import { createShortLink } from '../../store/slice/linkSlice'
+import { createShortLink, selectLoading, addLink } from '../../store/slice/linkSlice'
 import shorten from '../../images/bg-shorten-desktop.svg'
 import styles from './Form.module.css'
 
 const Form = () => {
+	const loading = useSelector(selectLoading)
 	const dispath = useDispatch()
 
 	const {
 		register,
 		formState: { errors },
-		handleSubmit
-		// reset
+		handleSubmit,
+		reset
 	} = useForm({
 		mode: 'onSubmit'
 	})
 
 	const onSubmit = ({ Url }) => {
-		// dispath(createShortLink(Url))
-
-		const response = fetch(`https://tinyurl.com/api-create.php?url=-${encodeURIComponent(Url)}`)
-		if (response.ok) {
-			const data = response.text()
-			console.log(data)
-		} else {
-			console.log(response)
-		}
-
-		// fetch('https://cleanuri.com/api/v1/shorten', {
-		// 	method: 'POST',
-		// 	headers: {
-		// 		'Content-Type': 'application/x-www-form-urlencoded'
-		// 	},
-		// 	body: new URLSearchParams({
-		// 		url: 'https://google.com/'
-		// 	})
-		// })
-		// 	.then(response => {
-		// 		if (!response.ok) {
-		// 			throw new Error('Network response was not ok ' + response.statusText)
-		// 		}
-		// 		return response.json()
-		// 	})
-		// 	.then(data => console.log(data))
-		// 	.catch(error => console.error('Error:', error))
+		dispath(createShortLink(Url))
+		dispath(addLink(Url))
+		reset()
+		// try {
+		// 	fetch(`${API_BASE_URL}?apikey=${API_KEY_URL}&url=${Url}=shrlc`)
+		// 		.then(response => response.json())
+		// 		.then(data => setLink(data))
+		// 	// return response.json()
+		// } catch (error) {
+		// 	console.log(error)
+		// }
+		// // dispath(addLink(createShortenLink(createShortLink(Url), Url)))
+		// reset()
+		// dispath(addLink(createShortenLink(link, Url)))
 	}
 
 	return (
@@ -56,7 +44,7 @@ const Form = () => {
 						type="url"
 						placeholder="Shorten a link here..."
 						className={styles.input}
-						style={{ border: errors.Url ? '1px solid red' : '1px solid transparent' }}
+						style={{ outlineColor: errors.Url ? 'red' : 'currentColor', outlineWidth: errors.Url ? '4px' : '1px' }}
 						{...register('Url', {
 							required: 'Please add a link',
 							pattern: {
@@ -65,8 +53,9 @@ const Form = () => {
 								message: 'Please enter a valid url'
 							}
 						})}
+						disabled={loading === 'loading'}
 					/>
-					<Button type="submit" className={styles.formBtn} text={'Shorten it!'} />
+					<Button type="submit" className={styles.formBtn} text={'Shorten it!'} disabled={loading === 'loading'} />
 				</form>
 				{errors.Url && <span className={styles.error}>{errors.Url.message}</span>}
 			</div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import BootomPage from '../components/BottomPage/BootomPage'
 import Features from '../components/Features/Features'
 import Hero from '../components/Hero/Hero'
 import Form from '../components/Form/Form'
+import Shortens from '../components/Shortens/Shortens'
 
 const HomePage = () => {
 	return (
 		<>
 			<Hero />
 			<Form />
+			<Shortens />
 			<Features />
 			<BootomPage />
 		</>
