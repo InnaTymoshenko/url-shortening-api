488aa4cc30ad4ace8f6709822e78c3b453c5b8ca
InnaTymoshenko <innatymosh@gmail.com>
2024-07-05 19:34:31 +0300

    minor fixed

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -7,7 +7,7 @@ const Features = () => {
 		<section className={styles.features}>
 			<div className={styles.container}>
 				<div className={styles.title}>
-					<h3>Advanced Statistics</h3>
+					<h2>Advanced Statistics</h2>
 					<p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
 				</div>
 				<Card />
