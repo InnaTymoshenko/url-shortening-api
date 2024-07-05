c9eaf2ff4ef51493df3c85d27b26c9ef7415126b
InnaTymoshenko <innatymosh@gmail.com>
2024-07-05 14:39:42 +0300

    added store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import linkReduces from './slice/linkSlice'
 
-export const store = configureStore({
-	reducer: {
-		links: linkReduces
-	}
+const persistConfig = {
+	key: 'root',
+	storage
+}
+
+const rootReduces = combineReducers({
+	links: linkReduces
 })
+
+const persistedReduces = persistReducer(persistConfig, rootReduces)
+
+export const store = configureStore({
+	reducer: persistedReduces,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+			}
+		})
+})
+
+export const persistor = persistStore(store)
--- src/store/slice/linkSlice.js
+++ src/store/slice/linkSlice.js
@@ -1,33 +1,44 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { API_BASE_URL } from '../../config'
+import { API_BASE_URL, API_KEY_URL } from '../../config'
 
-export const createShortLink = createAsyncThunk('links/createShortLink', async url => {
-	const response = await fetch(API_BASE_URL, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: `url=${url}`
-	})
-	console.log(response.json())
-	return await response.json()
+export const createShortLink = createAsyncThunk('links/createShortLink', async (url, thunkAPI) => {
+	try {
+		const response = await fetch(`${API_BASE_URL}?apikey=${API_KEY_URL}&url=${url}=shrlc`)
+		return await response.json()
+	} catch (error) {
+		return thunkAPI.rejectWithValue(error)
+	}
 })
 
 const initialState = {
 	items: [],
+	shorten: '',
+	url: '',
 	loading: 'idle'
 }
 
 const linkSlice = createSlice({
 	name: 'links',
 	initialState,
+	reducers: {
+		addLink: (state, action) => {
+			state.url = action.payload
+		},
+		createShorten: (state, action) => {
+			state.items.push(action.payload)
+			state.url = ''
+			state.shorten = ''
+		}
+	},
 	extraReducers: builder => {
 		builder.addCase(createShortLink.pending, state => {
 			state.loading = 'loading'
 		})
 		builder.addCase(createShortLink.fulfilled, (state, action) => {
-			const { result_url } = action.payload
-			console.log(action.payload)
-			if (result_url) {
-				state.items.push(result_url)
+			const { status_code, data } = action.payload
+			console.log(data)
+			if (status_code === '200') {
+				state.shorten = data
 				state.loading = 'idle'
 			} else {
 				state.loading = 'error'
@@ -38,5 +49,15 @@ const linkSlice = createSlice({
 		})
 	}
 })
+
+export const { addLink, createShorten } = linkSlice.actions
+
+export const selectLoading = state => state.links.loading
+
+export const selectItems = state => state.links.items
+
+export const selectUrl = state => state.links.url
+
+export const selectShorten = state => state.links.shorten
 
 export default linkSlice.reducer
